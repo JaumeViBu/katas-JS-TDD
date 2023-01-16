b7ba87a46f1e83c3f92e2d5d7fb99e824691fd24
jaume vidal <jaumevibu@gmail.com>
2023-01-16 15:01:42 +0100

    Added test for sumaPares

--- __tests__/retos01.test.js
+++ __tests__/retos01.test.js
@@ -9,11 +9,16 @@ test('maxNumber debe devolver el numero mayor de un array de numeros pasado', ()
   const sut = maxNumber(numList);
   //THEN
   expect(sut).toBe(3);
+  expect(typeof sut).toBe('number');
 });
 
 
-test('', () => {
+test('sumaPares, al darle una lista de números devuelva la suma de todos los que sean pares.', () => {
   //GIVEN
+  const numList = [1, -4, -5, 0, 3, 2];
   //WHEN
+  const sut = sumaPares(numList);
   //THEN
+  expect(sut).toBe(-2);
+  expect(typeof sut).toBe('number');
 });
