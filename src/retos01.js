a7f56024a1b69e4c8dd4773a294a05c67a66c956
Daniel-gallegO <danielgallegolopez@outlook.com>
2023-01-16 15:28:03 +0100

    added sumaPares to retos01.js

--- src/retos01.js
+++ src/retos01.js
@@ -1,15 +1,21 @@
 function maxNumber(list) {
-  max = list[0];
+  let max = list[0];
   for (let i = 1; i < list.length; i++) {
 
     max = list[i] > max ? list[i] : max;
   }
   return max;
-}
+};
 
 function sumaPares(list) {
-
-}
+  let suma = 0;
+    for (const item of list) {
+      if (item%2 == 0) suma += item;
+      // suma += item%2==0 ? item:0;
+    
+  }
+  return suma;
+};
 
 module.exports = {
   maxNumber,
